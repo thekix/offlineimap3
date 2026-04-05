4d300783c7c29c79014a47197515aaea594ac2b8
Derek Schrock <dereks@lifeofadishwasher.com>
2026-04-05 19:34:12 -0400

    Use correct IMAP folder name with  imapobj.select
    Like other imapobj.select translate it vi
    imaputil.foldername_to_imapname

--- offlineimap/imapserver.py
+++ offlineimap/imapserver.py
@@ -890,7 +890,7 @@ class IdleThread:
             while not success:
                 imapobj = self.parent.acquireconnection()
                 try:
-                    imapobj.select(self.folder)
+                    imapobj.select(imaputil.foldername_to_imapname(self.folder))
                 except OfflineImapError as e:
                     if e.severity == OfflineImapError.ERROR.FOLDER_RETRY:
                         # Connection closed, release connection and retry.
